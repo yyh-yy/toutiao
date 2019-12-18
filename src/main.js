65968c5f8cbe877730adb0437c938f2abf1cfc4b
yaoyuhui <435607052@qq.com>
2019-12-18 20:44:40 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import ElementUI from 'element-ui'
-
+import './style/index.less'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
