59ceadfa57785ea110b58c6a83d3d654012ea8db
yaoyuhui <435607052@qq.com>
2019-12-19 11:20:33 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,20 +4,20 @@
     <div class="title">
         <img src="../../assets/img/logo_index.png" alt="">
     </div>
-    <el-form>
+    <el-form :model="loginForm" :rules="rules" ref='myForm' >
         <!-- 表单域 -->
-        <el-form-item>
-            <el-input v-model="input" placeholder="请输入手机号"></el-input>
+        <el-form-item prop='mobile'>
+            <el-input  placeholder="请输入手机号" v-model="loginForm.mobile"></el-input>
         </el-form-item >
-              <el-form-item>
-            <el-input v-model="input" placeholder="请输入验证码" style='width:65%'></el-input>
+              <el-form-item prop='code'>
+            <el-input  placeholder="请输入验证码" style='width:65%' v-model="loginForm.code"></el-input>
             <el-button plain style="float:right">获取验证码</el-button>
         </el-form-item>
-              <el-form-item>
-            <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+              <el-form-item prop="check">
+            <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item >
         <el-form-item>
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button type="primary" @click='submitForm' style="width:100%">登录</el-button>
         </el-form-item>
 
     </el-form>
@@ -27,6 +27,41 @@
 
 <script>
 export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      rules: {
+        mobile: [{ required: true, message: '请输入您的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '输入有误' }
+        ],
+        code: [{ required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '请输入6位验证码' }
+        ],
+        check: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('未同意注册'))
+          }
+        } }]
+
+      }
+
+    }
+  },
+  methods: {
+    submitForm () {
+      this.$refs.myForm.validate((isOk) => {
+        if (isOk) {
+          alert('登录成功')
+        }
+      })
+    }
+  }
 
 }
 </script>
