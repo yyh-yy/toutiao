651dee2ea8e17d0f2134078be840fa55a2097dae
yaoyuhui <435607052@qq.com>
2019-12-20 16:49:36 +0800

    侧边栏

---
+++ src/components/home/sidebar.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="side-bar">
+    <div class="title">
+        <img src="../../assets/img/logo_admin.png" alt="">
+    </div>
+<el-menu background-color='#353b4e' text-color='#adafb5' class="el-menu" active-text-color="#ffd04b">
+    <el-menu-item>首页</el-menu-item>
+    <el-submenu>
+        <template slot='title'>文章管理</template>
+        <el-menu-item>发布文章</el-menu-item>
+        <el-menu-item>文章列表</el-menu-item>
+        <el-menu-item>评论管理</el-menu-item>
+        <el-menu-item>素材管理</el-menu-item>
+    </el-submenu>
+    <el-submenu>
+        <template slot='title'>粉丝管理</template>
+        <el-menu-item>图文数据</el-menu-item>
+        <el-menu-item>粉丝概况</el-menu-item>
+        <el-menu-item>粉丝画像</el-menu-item>
+        <el-menu-item>粉丝列表</el-menu-item>
+    </el-submenu>
+    <el-menu-item>账户信息</el-menu-item>
+</el-menu>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.side-bar{
+    width: 300px;
+    .el-menu {
+        border-right: none;
+    }
+    .title {
+        background-color: #2e2f32;
+        text-align: center;
+        img {
+            height: 35px;
+        }
+        padding: 10px 0 ;
+    }
+}
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>主页</div>
+ <el-container>
+<el-aside>
+<side-bar style="height:100vh;background-color:#353b4e"></side-bar>
+</el-aside>
+ <el-container>
+<el-header>顶部</el-header>
+ <el-main>内容</el-main>
+   </el-container>
+ </el-container>
 </template>
 
 <script>
+import sidebar from '../../components/home/sidebar'
 export default {
+  components: {
+    'side-bar': sidebar
 
+  }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.nav{
+  width: 230px;
+  .title {
+    background-color: #2e2f32;
+    text-align: center;
+    img {
+      height: 35px;
+    }
 
+  }
+
+}
 </style>
