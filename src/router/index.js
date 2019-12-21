5b57257eee08903fef2f9fa5bbb38c1d5f2543a7
yaoyuhui <435607052@qq.com>
2019-12-21 10:29:20 +0800

    主页模块-默认导航

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home'
 import Login from '../views/login'
-
+import Home2 from '../views/home/home.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -10,7 +10,12 @@ const routes = [
   {
     path: '/home',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [{
+      path: '',
+      component: Home2
+    }]
+
   },
   {
     path: '/login',
