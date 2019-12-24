6fa981ccdd7e433e280bfba5f5ff1abde93f7463
yaoyuhui <435607052@qq.com>
2019-12-24 10:38:24 +0800

    头条PC-素材列表-页面结构-tab

---
+++ src/views/material/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-card>
+      <bread-crumb slot="header">
+      <template slot="title"></template>
+      </bread-crumb>
+            <el-tabs v-model="activeName">
+        <el-tab-pane label="全部图片" name="all"></el-tab-pane>
+        <el-tab-pane label="收藏图片" name="collect"></el-tab-pane>
+      </el-tabs>
+  </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
