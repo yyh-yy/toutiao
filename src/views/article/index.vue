1c250b12daee5de61f617bcb5c221b51824f48bb
yaoyuhui <435607052@qq.com>
2019-12-25 09:12:47 +0800

    头条PC-内容列表-页面主体结构

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,13 +1,46 @@
 <template>
-  <el-card>文章列表</el-card>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">文章列表</template>
+    </bread-crumb>
+    <el-form style="padding-left:55px">
+      <el-form-item label="文章状态:">
+        <el-radio-group v-model="searchForm.status">
+          <!-- 放置一个单选组  文章状态，0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部-->
+          <el-radio :label="4">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道列表:">
+        <el-select placeholder="请选择频道"></el-select>
+      </el-form-item>
+      <el-form-item label="时间选择:">
+        <el-date-picker
+          v-model="value1"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      searchForm: {
 
+      }
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
