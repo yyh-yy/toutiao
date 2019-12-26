36d64f7d4849f19ca1ee16616491f006434f436c
yaoyuhui <435607052@qq.com>
2019-12-26 15:55:59 +0800

    头条PC-内容列表-分页请求

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -16,15 +16,13 @@
       </el-form-item>
       <el-form-item label="频道列表:">
         <el-select placeholder="请选择频道" v-model="searchForm.channel_id" @change="changeCondition">
-            <el-option v-for="item in channels" :key='item.id' :label="item.name" :value="item.id">
-
-            </el-option>
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="时间选择:" >
+      <el-form-item label="时间选择:">
         <el-date-picker
-        @change="changeCondition"
-        v-model="searchForm.dateRange"
+          @change="changeCondition"
+          v-model="searchForm.dateRange"
           type="daterange"
           range-separator="-"
           start-placeholder="开始日期"
@@ -34,25 +32,35 @@
       </el-form-item>
     </el-form>
     <el-row class="total" type="flex" align="middle">
-        <span>共找到{{}}条符合条件的内容</span>
+      <span>共找到{{page.total}}条符合条件的内容</span>
     </el-row>
-    <div class="article-item" v-for="item in list" :key='item.id.toString()'>
-        <!-- 左侧 -->
-        <div class="left">
-            <img :src="item.cover.images.length ? item.cover.images[0]:defaultImg" alt="">
-            <div class="info">
-                <span>{{item.title}}</span>
-                <el-tag :type='item.status | filterType'
-                class="tag" style="width:80px; text-align:center">{{item.status | filterStatus}}</el-tag>
-                <span class="date">{{item.pubdate}}</span>
-
-            </div>
+    <div class="article-item" v-for="item in list" :key="item.id.toString()">
+      <!-- 左侧 -->
+      <div class="left">
+        <img :src="item.cover.images.length ? item.cover.images[0]:defaultImg" alt />
+        <div class="info">
+          <span>{{item.title}}</span>
+          <el-tag
+            :type="item.status | filterType"
+            class="tag"
+            style="width:80px; text-align:center"
+          >{{item.status | filterStatus}}</el-tag>
+          <span class="date">{{item.pubdate}}</span>
         </div>
-        <div class="right">
-            <i class="el-icon-edit">修改</i>
-            <i class="el-icon-delete">删除</i>
-        </div>
+      </div>
+      <div class="right">
+        <i class="el-icon-edit">修改</i>
+        <i class="el-icon-delete">删除</i>
+      </div>
     </div>
+    <el-row type="flex" justify="center" style="height:80px" align="middle">
+      <el-pagination background layout="prev, pager, next"
+      :total="page.total" :page-size="page.pageSize"
+      :current-page="page.currentPage"
+      @current-change='changePage'
+      ></el-pagination>
+    </el-row>
+
   </el-card>
 </template>
 
@@ -95,22 +103,38 @@ export default {
         status: 5,
         channel_id: null,
         dateRange: []
-
       },
       channels: [],
       list: [],
-      defaultImg: require('../../assets/img/avatar.jpg')
+      defaultImg: require('../../assets/img/avatar.jpg'),
+      page: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0
+      }
     }
   },
   methods: {
+    // 分页
+    changePage (newPage) {
+      console.log(newPage)
+
+      this.page.currentPage = newPage
+      this.getConditionArticle()
+    },
     //   搜索筛选
     changeCondition () {
+      this.page.currentPage = 1
+      this.getConditionArticle()
+    },
+    getConditionArticle () {
       let params = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize,
         status: this.searchForm.status === 5 ? null : this.searchForm.status,
         channel_id: this.searchForm.channel_id,
         begin_pubdate: this.searchForm.dateRange.length ? this.searchForm.dateRange[0] : null,
         end_pubdate: this.searchForm.dateRange.length > 1 ? this.searchForm.dateRange[1] : null
-
       }
       this.getArticles(params)
     },
@@ -119,17 +143,15 @@ export default {
       this.$http({
         url: '/articles',
         params
-
       }).then(res => {
-        console.log(res)
         this.list = res.data.results
+        this.page.total = res.data.total_count
       })
     },
     //   获取文章频道
     getChannels () {
       this.$http({
         url: '/channels'
-
       }).then(res => {
         this.channels = res.data.channels
       })
@@ -138,48 +160,49 @@ export default {
   created () {
     this.getChannels()
     this.getArticles()
+    this.getConditionArticle({ page: 1, per_page: 10 })
   }
 }
 </script>
 
 <style lang='less' scoped>
-.articles{
-    .total {
-        height: 60px;
-        border-bottom: 1px dashed #999;
+.articles {
+  .total {
+    height: 60px;
+    border-bottom: 1px dashed #999;
+  }
+  .article-item {
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 0;
+    border-bottom: 1px solid #f2f3f5;
+  }
+  .left {
+    display: flex;
+    img {
+      width: 150px;
+      height: 150px;
+      border-radius: 4px;
     }
-    .article-item {
-        display: flex;
-        justify-content: space-between;
-        padding: 20px 0;
-        border-bottom: 1px solid #f2f3f5;
+    .info {
+      height: 100px;
+      margin-left: 10px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      .date {
+        color: #999;
+        font-size: 12px;
+      }
     }
-    .left {
-        display: flex;
-        img {
-            width: 150px;
-            height: 150px;
-            border-radius: 4px;
-        }
-        .info {
-            height: 100px;
-            margin-left: 10px;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-around;
-            .date{
-                color: #999;
-                font-size: 12px;
-            }
-        }
+  }
+  .right {
+    font-size: 16px;
+    margin-right: 8px;
+    cursor: pointer;
+    i {
+      margin-right: 5px;
     }
-    .right {
-        font-size: 16px;
-        margin-right: 8px;
-        cursor: pointer;
-        i {
-            margin-right: 5px
-        }
-    }
+  }
 }
 </style>
