b4efb758ee1d4a43c1ec8d9fa079bde7eef18881
yaoyuhui <435607052@qq.com>
2019-12-28 11:50:03 +0800

    头条PC-发布文章-封面图片选择组件上传

--- src/components/publish/select-image.vue
+++ src/components/publish/select-image.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="cover-image">
     <el-tabs v-model="activeName" style="width:100%">
-    <el-tab-pane label="图片管理" name="first">
-      <div class="select-image-list">
-        <el-card v-for="item in list" :key="item.id" class="img-card">
-          <img :src="item.url" alt="" @click="clickImg(item.url)">
-        </el-card>
-      </div>
-    </el-tab-pane>
-    <el-tab-pane label="上传图片" name="second">上传图片</el-tab-pane>
-    <el-row type="flex" justify="center" style="margin-top:40px">
-<el-pagination
-  background
-  layout="prev, pager, next"
-  :page-size="page.pageSize"
-  :current-page="page.currentPage"
-  :total="page.total"
-  @current-change="changePage">
-</el-pagination>
-</el-row>
-  </el-tabs>
+      <el-tab-pane label="图片管理" name="first">
+        <div class="select-image-list">
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <img :src="item.url" alt @click="clickImg(item.url)" />
+          </el-card>
+          <el-row type="flex" justify="center" style="margin-top:40px">
+            <el-pagination
+              background
+              layout="prev, pager, next"
+              :page-size="page.pageSize"
+              :current-page="page.currentPage"
+              :total="page.total"
+              @current-change="changePage"
+            ></el-pagination>
+          </el-row>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="上传图片" name="second">
+        <el-upload
+          class="avatar-uploader"
+          action=""
+          :show-file-list="false"
+          :http-request="uploadImg "
+        >
+
+          <i class="el-icon-plus"></i>
+        </el-upload>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -37,6 +47,19 @@ export default {
     }
   },
   methods: {
+    // 上传图片
+    uploadImg (params) {
+      let data = new FormData()
+      data.append('image', params.file)
+      this.$http({
+        url: '/user/images',
+        method: 'post',
+        data
+
+      }).then(res => {
+        this.$emit('selectOneImge', res.data.url)
+      })
+    },
     // 点击图片传给父级
     clickImg (url) {
       this.$emit('selectOneImge', url)
@@ -48,7 +71,11 @@ export default {
     getImage () {
       this.$http({
         url: '/user/images',
-        params: { collect: false, page: this.page.currentPage, per_page: this.page.pageSize }
+        params: {
+          collect: false,
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
+        }
       }).then(res => {
         this.list = res.data.results
         this.page.total = res.data.total_count
@@ -58,24 +85,32 @@ export default {
   created () {
     this.getImage()
   }
-
 }
 </script>
 
 <style lang='less' scoped>
-.select-image-list{
+.select-image-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  .img-card {
+    width: 150px;
+    height: 150px;
+    margin-top: 20px;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+.avatar-uploader {
 display: flex;
-flex-wrap: wrap;
-justify-content: space-around;
-.img-card {
- width: 150px;
- height: 150px;
- margin-top: 20px
-}
-img {
-  width: 100%;
-  height: 100%;
+justify-content: center;
+i {
+font-size: 50px;
+padding: 50px;
+border: 1px dashed #ccc;
+border-radius: 4px;
 }
 }
-
 </style>
