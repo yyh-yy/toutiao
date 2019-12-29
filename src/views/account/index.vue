6d3f2eccd954d2bea34e5a025f77d51a113a37a0
yaoyuhui <435607052@qq.com>
2019-12-29 15:59:49 +0800

    头条PC-账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,16 +5,16 @@
     </bread-crumb>
     <el-form style="margin-left:100px" label-width="100px">
         <el-form-item label="用户名：">
-            <el-input style="width:35%"></el-input>
+            <el-input style="width:35%" v-model="formData.name"></el-input>
         </el-form-item>
                 <el-form-item label="简介：">
-            <el-input style="width:35%"></el-input>
+            <el-input style="width:35%" v-model="formData.intro"></el-input>
         </el-form-item>
                 <el-form-item label="邮箱">
-            <el-input style="width:35%"></el-input>
+            <el-input style="width:35%" v-model="formData.email"></el-input>
         </el-form-item>
                 <el-form-item label="手机号">
-            <el-input style="width:35%"></el-input>
+            <el-input style="width:35%" v-model="formData.mobile" disabled></el-input>
         </el-form-item>
         <el-form-item>
             <el-button type="primary">保存信息</el-button>
@@ -29,6 +29,29 @@
 
 <script>
 export default {
+  data () {
+    return {
+      formData: {
+        name: '',
+        intro: '',
+        email: '',
+        photo: '',
+        mbile: ''
+      }
+    }
+  },
+  methods: {
+    getUserInfo () {
+      this.$http({
+        url: '/user/profile'
+      }).then(res => {
+        this.formData = res.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 
 }
 </script>
