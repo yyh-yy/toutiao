8f1c72ae5ad07a03b38f14fb8e4e07c076caf1d7
yaoyuhui <435607052@qq.com>
2019-12-29 15:50:18 +0800

    头条PC-账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,8 +1,29 @@
 <template>
 <el-card>
     <bread-crumb slot="header">
-    <template title="账户信息"></template>
+    <template slot="title">账户信息</template>
     </bread-crumb>
+    <el-form style="margin-left:100px" label-width="100px">
+        <el-form-item label="用户名：">
+            <el-input style="width:35%"></el-input>
+        </el-form-item>
+                <el-form-item label="简介：">
+            <el-input style="width:35%"></el-input>
+        </el-form-item>
+                <el-form-item label="邮箱">
+            <el-input style="width:35%"></el-input>
+        </el-form-item>
+                <el-form-item label="手机号">
+            <el-input style="width:35%"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary">保存信息</el-button>
+        </el-form-item>
+    </el-form>
+    <el-upload class="head-upload" action="" :show-file-list="false">
+        <img src="../../assets/img/avatar.jpg" alt="">
+
+    </el-upload>
 </el-card>
 </template>
 
@@ -12,6 +33,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.head-upload {
+position: absolute;
+right: 300px;
+top: 200px;
+img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%
 
+}
+}
 </style>
